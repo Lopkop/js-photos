4ff90060387996f66fccf53a520e74dd52887ba1
Lopkop <lopkophacked@protonmail.com>
2024-05-26 13:38:13 +0300

    release 1.0

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const User = require('./models');
-const fs = require('fs')
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
-app.use(express.json())
+const User = require("./models");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cors());
+
+
+var util = require('util');
+var exec = require('child_process').exec;
+
 
 app.post('/register', async (req, res) => {
     let { username, photo_url } = req.body;
@@ -13,15 +21,54 @@ app.post('/register', async (req, res) => {
     }).save()
 })
 
+// // UPLOAD IMAGE
+uploadImage = async (req, res) => {
+    try {
+      const image = req.file ? req.file.filename : null;
+      const { name } = req.body;
+      const user = await User.findOne({ name });
+      user.photo = req.file.filename
+      var command = `curl -F 'file=@"public/images/${req.file.filename}"' http://localhost:5000`
+      child = exec(command, function(error, stdout, stderr){
+
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        
+        if(error !== null)
+        {
+            console.log('exec error: ' + error);
+        }
+        
+        });
+
+      res.status(201).json({ message: "data upload successfully" });
+    } catch (error) {
+      res.status(500).json({ error, message: "internal server error " });
+    }}
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./public/images");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload_photo", upload.single("image"), uploadImage);
+
+
+// ENDPOINT FOR GETTING URL TO A PHOTO
 app.get('/get_photo', async (req, res) => {
     let { username } = req.body;
     let user = await User.findOne({username});
     
-    fs.readFile('photos/'+user.photo, (err, data) => {
-        res.writeHead(200, {'Content-Type': 'image/jpeg'})
-        res.end(data)
-    }
-    )})
+    res.status(200).send('http://localhost:5000/'+user.photo)
+})
 
 
 module.exports = app;
--- start.js
+++ start.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose');
 const app = require('./main');
 
 mongoose.connect("mongodb://localhost:27017/yu").then(() => console.log("Database connected!")).catch(err => console.log(err));
-app.listen(5000, () => console.log('listening'))
+app.listen(8080, () => console.log('listening'))
